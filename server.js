c656da84d7453bd141f6c9105dd801007fc42946
Amira Zrelli <emira.zrelli@gmail.com>
2021-11-20 12:00:32 +0100

    done

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 //** */
+
 const express = require("express");
+const path = require("path");
 const connectDB = require("./config/connectDB");
 require("dotenv").config();
 //************************** *
@@ -14,6 +16,15 @@ app.use("/api/topic", require("./router/TopicList"));
 app.use("/api/forum", require("./router/Post"));
 // app.use("/", require("./router/user"));
 //************************** */
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT;
 
 app.listen(PORT, (err) =>
